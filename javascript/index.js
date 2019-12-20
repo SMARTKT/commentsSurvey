06b27fa6a9b91609ba866b211ca8dfb099e33ea7
Ayush <abansal1008@gmail.com>
2019-12-21 01:13:17 +0530

    Update js and css

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,19 @@
-var CODE_ID = "1";
+var CODE_ID = document.getElementById("commentCodeID").innerHTML;
+console.log(CODE_ID);
 var clickedBtn = "";
+var email = "";
+var name = "";
+var designation = "";
+var affiliation = "";
 function openForm(btnId) {
 	if (clickedBtn != "")
 	{
 		document.getElementById(clickedBtn).style.background = "";
 	}
+	document.getElementById("emailField").value = email;
+	document.getElementById("nameField").value = name;
+	document.getElementById("designationField").value = designation;
+	document.getElementById("affiliationField").value = affiliation;
 	clickedBtn = btnId;
 	document.getElementById(btnId).style.background = "red";
   	document.getElementById("myForm").style.display = "block";
@@ -26,12 +35,22 @@ function postToGoogle() {
 	if (document.getElementById('commentNotUseful').checked) {
 		field2 = "NU";
 	}
+	if (document.getElementById('commentPartialUseful').checked) {
+		field2 = "PU";
+	}
 	var field3 =  $("#commentView").val();
 	var field4 = $("#commentAlternate").val();
 
+	email = $("#emailField").val();
+	name = $("#nameField").val();
+	designation = $("#designationField").val();
+	affiliation = $("#affiliationField").val();
+
+
 	$.ajax({
 	    url: "https://docs.google.com/forms/d/e/1FAIpQLSfFptUsvMpu-bhgJRuWcxtsTY5arq1IRLDeiWMQrfLY0ucRGg/formResponse?",
-		data: {"entry.1036359742": field1, "entry.1158064626": field2, "entry.841187499": field3, "entry.698127407": field4},
+		data: {"entry.1036359742": field1, "entry.1158064626": field2, "entry.841187499": field3, "entry.698127407": field4,
+				"entry.1336341702": email, "entry.728973787": name, "entry.650664319": designation, "entry.804634005": affiliation},
 	    type: "POST",
 	    dataType: "xml",
 	    success: function(d)
